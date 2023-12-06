98188f5260da777be70de7dd0cf772aef2bcc001
deepanshi2704 <deepanshigupta2704@gmail.com>
2023-12-06 14:19:46 +0530

    alpha seventh commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGskgURHo0Wxqis5lJ1xvAEyTG7BIrUBk",
+  authDomain: "fwm-firebase.firebaseapp.com",
+  projectId: "fwm-firebase",
+  storageBucket: "fwm-firebase.appspot.com",
+  messagingSenderId: "605331128221",
+  appId: "1:605331128221:web:325187b4f99c40eb12b974"
+};
